f6e6b51cd978f7c16b3695546951ac5dd1d460cf
Guillermo Molina <gmolinav2005@gmail.com>
2023-08-02 10:50:08 -0600

    primer commit

---
+++ classes/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+class Database
+{
+
+    private $db_host = 'localhost';
+    private $db_name = 'php_auth_api';
+    private $db_username = 'root';
+    private $db_password = 'lepanto';
+
+    public function dbConnection() {
+        try {
+
+            $conn = new PDO('mysql:host='.$this->db_host.';db_name='.$this->db_name,$this->db_username,$this->db_password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $conn;
+
+        }
+        catch (PDOException $e) {
+            echo "Connection error: " . $e->getMessage();
+        } finally {
+            exit;
+        }
+    }
+
+}
+
